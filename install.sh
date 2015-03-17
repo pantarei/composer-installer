f28085132d12009a36d77a1f1d2f3a8a91a3c506
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-03-17 11:11:56 +0800

    Expect composer already installed globally.

--- install.sh
+++ install.sh
@@ -6,16 +6,6 @@ set -o xtrace
 BRANCH="master"
 TMP_DIR=`mktemp -d -t composer-installer.XXXXXX`
 
-# Install composer locally.
-mkdir -p $TMP_DIR
-curl -sS https://getcomposer.org/installer | php -- --install-dir=$TMP_DIR
-
-mkdir -p $HOME/bin
-mv $TMP_DIR/composer.phar $HOME/bin/composer
-
-export PATH=$HOME/bin:$PATH
-composer selfupdate
-
 # Fetch package list.
 mkdir -p $HOME/.composer
 curl -sL https://raw.githubusercontent.com/pantarei/composer-installer/$BRANCH/composer.json > $HOME/.composer/composer.json
