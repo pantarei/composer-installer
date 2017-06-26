9ad13d174a4cc06ce4cfd815994635101240786a
Wong Hoi Sing Edison <hswong3i@gmail.com>
2017-06-26 09:41:24 +0800

    cgr update

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ composer global require --no-update hirak/prestissimo:@stable
 composer global update
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
-# Install/Update packages.
+# Install packages.
 cgr drupal/coder              ~8.0
 cgr drupal/console            ~1.0.0-rc19 --stability RC
 cgr drush/drush               ~8.0
@@ -22,6 +22,9 @@ cgr phpunit/phpunit           ~5.7
 cgr sami/sami                 ~4.0
 cgr satooshi/php-coveralls    ~1.0
 cgr squizlabs/php_codesniffer ~3.0
+
+# Update packages.
+cgr update
 
 # Register the Drupal and DrupalPractice Standard with PHPCS:
 phpcs --config-set installed_paths ~/.composer/global/drupal/coder/vendor/drupal/coder/coder_sniffer
